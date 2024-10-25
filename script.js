b4cf31032eef4418e381a3077d6baccae7080948
Caio Souza <ocaiomoises@gmail.com>
2024-10-25 07:54:34 -0300

    create - enter keypress

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-// capture only words (regexp)
 // When press "enter", execute
+// capture only words (regexp)
 // don't show sugestions
 
 const textInput = document.querySelector("#text-input");
 const checkButton = document.querySelector("#check-btn");
+
+
+textInput.addEventListener("keypress", () => {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        alert("Raleu mofi");
+    }
+});
 
 checkButton.addEventListener("click", () => {
     const inputValue = textInput.value;
@@ -13,10 +21,12 @@ checkButton.addEventListener("click", () => {
         // Pass all words to lower case
         const inputValueLowerCase = inputValue.toLowerCase();
 
+        // ([A-Z]|[a-z])\w+
+
         // Transform words to arrays
         const inputArray = Array.from(inputValueLowerCase);
         const reversedArray = inputArray.toReversed();
-        
+
         // Check if working
         alert(inputArray)
         alert(`Array reverso: ${reversedArray}`);
