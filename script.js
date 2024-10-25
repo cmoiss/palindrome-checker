c906374d8a2175163d2c7ce6ed1f4b1711a321b3
Caio Souza <ocaiomoises@gmail.com>
2024-10-25 07:23:17 -0300

    get reverse array

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// pass all to lower case
+// capture only words (regexp)
+// When press "enter", execute
+// don't show sugestions
+
 const textInput = document.querySelector("#text-input");
 const checkButton = document.querySelector("#check-btn");
 
@@ -7,7 +12,9 @@ checkButton.addEventListener("click", () => {
     // Checks if something was typed in the input
     if (inputValue) {
         const inputArray = Array.from(inputValue);
+        const reversedArray = inputArray.toReversed();
         alert(inputArray)
+        alert(`Array reverso: ${reversedArray}`);
     } else {
         alert("Please input a value");
     }    
